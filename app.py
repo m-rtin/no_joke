6dd40bf49b253770c4b3e923724f56e3951e161d
m-rtin <m-rtin@gmx.net>
2020-06-07 14:35:39 +0200

    Added predictions

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request, session
+import pickle
 
+from flask import Flask, render_template, request, session
+from tensorflow.keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+
+MAX_LEN = 50
 
 app = Flask(__name__)
-app.secret_key = 'any random string'
 
 
 @app.route('/')
@@ -14,10 +18,18 @@ def form():
 def result():
     if request.method == 'POST':
         input_text = request.form["input_text"]
-        return render_template("result.html", result=input_text)
+        prediction = round(predict(input_text)*100, 2)
+        return render_template("result.html", result=prediction)
 
 
+def predict(text):
+    x_test = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=MAX_LEN)
+    score = model.predict([x_test])[0]
+    return score[0]
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+    model = load_model("model.h5")
+    app.run(threaded=False, debug=True)
