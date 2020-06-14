108b9ba9eb7a939c5f6d02981263556ddc43b8cd
m-rtin <m-rtin@gmx.net>
2020-06-14 17:47:29 +0200

    Added RNN

--- app.py
+++ app.py
@@ -18,13 +18,16 @@ def form():
 def result():
     if request.method == 'POST':
         input_text = request.form["input_text"]
-        prediction = round(predict(input_text)*100, 2)
+        if request.form.get("model_selection") == "NN":
+            prediction = round(predict(input_text, model)*100, 2)
+        elif request.form.get("model_selection") == "RNN":
+            prediction = round(predict(input_text, rnn_model)*100, 2)
         return render_template("result.html", result=prediction)
 
 
-def predict(text):
+def predict(text, selected_model):
     x_test = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=MAX_LEN)
-    score = model.predict([x_test])[0]
+    score = selected_model.predict([x_test])[0]
     return score[0]
 
 
@@ -32,4 +35,5 @@ if __name__ == '__main__':
     with open('tokenizer.pickle', 'rb') as handle:
         tokenizer = pickle.load(handle)
     model = load_model("model.h5")
+    rnn_model = load_model("rnnmodel.h5")
     app.run(threaded=False, debug=True)
